e8fd7083ae40d35ed14f99102e218ef1df398b7c
Lilian Cristine Semann <126900345+LilianSemann@users.noreply.github.com>
2023-06-26 15:05:46 -0300

    26-06
    design

--- src/app/menu/index.tsx
+++ src/app/menu/index.tsx
@@ -9,26 +9,24 @@ export default function ActiveHover({ id, children }) {
     const isActive = id === segment;
 
     return (
-        <Wrapper>
-            <Link
-                href={`/menu/${id}`}
-                style={{color: isActive ? "white" : "black"}}
-            >
+        <Wrapper active={isActive}>
+            <Link href={`/menu/${id}`}>
                 {children}
             </Link>
         </Wrapper>
     )
 }
 
-const Wrapper = styled.div`
+const Wrapper = styled.div<{active: boolean}>`
     a {
         padding: 0.5em 1em;
+        color: ${props=>props.active?"white":"black"};
         text-decoration: none;
-        background-color: lightgrey;
+        background-color: ${props=>props.active?"green":"#c4c4c4"};
         border-radius: 20px;
         transition: 0.2s;
         &:hover {
-            background-color: #39ac39;
+            background-color: #24b124;
             color: white;
         }
     }
